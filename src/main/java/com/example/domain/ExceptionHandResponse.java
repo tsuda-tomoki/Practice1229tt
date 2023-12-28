eb74db88cae0307754c318fc1323ddc7ea63d48a
Tomoki TSUDA <138654305+tsuda-tomoki@users.noreply.github.com>
2023-12-28 20:48:38 +0900

    例外の修正2

--- src/main/java/com/example/domain/ExceptionHandResponse.java
+++ src/main/java/com/example/domain/ExceptionHandResponse.java
@@ -1,6 +1,11 @@
 package com.example.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
-public record ExceptionHandResponse(String code, String message, List<Details> detailsList) {
+public record ExceptionHandResponse(
+        @JsonProperty("code") String code,
+        @JsonProperty("message") String message,
+        @JsonProperty("details") List<Details> detailsList) {
 }
